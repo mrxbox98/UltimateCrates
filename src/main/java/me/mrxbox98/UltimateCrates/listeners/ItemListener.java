2fac425d688dc3d8fc4aac98dbb5489d9237d116
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2021-08-31 15:39:23 -0700

    Update ItemListener.java

--- src/main/java/me/mrxbox98/UltimateCrates/listeners/ItemListener.java
+++ src/main/java/me/mrxbox98/UltimateCrates/listeners/ItemListener.java
@@ -1,6 +1,20 @@
 package me.mrxbox98.UltimateCrates.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ItemListener implements Listener {
+
+    @EventHandler
+    public void onRightClick(PlayerInteractEvent event)
+    {
+        if(event.getItem()==null)
+        {
+            return;
+        }
+
+        
+    }
+
 }
