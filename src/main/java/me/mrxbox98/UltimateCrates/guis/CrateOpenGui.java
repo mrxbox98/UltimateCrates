82d1208853584a83b051902edc9da1a6bf07f4d7
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2021-09-02 15:16:53 -0700

    Update CrateOpenGui.java

--- src/main/java/me/mrxbox98/UltimateCrates/guis/CrateOpenGui.java
+++ src/main/java/me/mrxbox98/UltimateCrates/guis/CrateOpenGui.java
@@ -1,10 +1,16 @@
 package me.mrxbox98.UltimateCrates.guis;
 
+import me.mrxbox98.UltimateCrates.UltimateCrates;
 import me.mrxbox98.UltimateCrates.crates.Crate;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
 
 public class CrateOpenGui implements Listener {
 
@@ -19,13 +25,39 @@ public class CrateOpenGui implements Listener {
         this.player=player;
         this.crate=crate;
 
-        inventory= Bukkit.createInventory(null,54,"Opening a crate!");
+        inventory= Bukkit.createInventory(null,45,"Opening a crate!");
 
         animate();
     }
 
     public void animate()
     {
+        for(int i=0;i<22;i++)
+        {
+            int finalI = i;
+            Bukkit.getScheduler().runTaskLater(UltimateCrates.instance, new Runnable() {
+                @Override
+                public void run() {
+                    inventory.setItem(finalI,generateRandomGlass());
+                    inventory.setItem(45- finalI,generateRandomGlass());
+                }
+            },i*4);
+        }
 
+        Bukkit.getScheduler().runTaskLater(UltimateCrates.instance, new Runnable() {
+            @Override
+            public void run() {
+                inventory.setItem(22,crate.roll().getItemStack());
+            }
+        },22*4);
+    }
+
+    public ItemStack generateRandomGlass()
+    {
+        ItemStack itemStack = new ItemStack(Material.STAINED_GLASS_PANE,1,(byte)new Random().nextInt(14));
+
+        itemStack.getItemMeta().setDisplayName(ChatColor.DARK_PURPLE+"...");
+
+        return itemStack;
     }
 }
